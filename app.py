567d50f3577339b0d48ed157bb2e0dc52415466c
Aagam Bakliwal <aagam.bos@gmail.com>
2023-03-26 17:02:55 +0530

    Signup for an admin, addition of new company members for that admin is done

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask, render_template, request
-import matplotlib
-import sys
-import math
-import os
+from flask import Flask, render_template, request, url_for, redirect, session
 import sqlite3
+import sys
 
-matplotlib.use('Agg') 
+uname = None
+company_name = None
+company_id = None
+
+d = {"approver":2, "accounts":3, "vendor":4}
 
 def getdbConnection():
     conn = sqlite3.connect('users.db')
@@ -21,6 +22,9 @@ def validate_passwords(pwd1, pwd2):
 
 
 app = Flask(__name__)
+app.secret_key = "my_secret_key"
+
+
 
 @app.route('/')
 def index():
@@ -59,12 +63,13 @@ def storeAccountData():
     
     return render_template("login.html", message="Account created successfully! Please login to continue")
 
-
+# This is used to determine which dashboard page should be loaded after authentication
 @app.route('/loginDashboard', methods=['POST'])
 def loginDashboard():
+    global uname
     uname = request.form.get("uname")
     pwd = request.form.get("pwd")
-    role = request.form.get("role")
+    # role = request.form.get("role")
     
     print("USERNAME AND PASSWORD: ", uname, pwd, file=sys.stderr)
     
@@ -72,50 +77,133 @@ def loginDashboard():
     conn = sqlite3.connect('users.db')
     cur = conn.cursor()
 
-    # Create a table for storing user credentials
-    if role == 0:
-        conn.execute('''CREATE TABLE IF NOT EXISTS approver
-                    (id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    fname TEXT NOT NULL,
-                    lname TEXT NOT NULL,
-                    cname TEXT NOT NULL UNIQUE,
-                    uname TEXT NOT NULL UNIQUE,
-                    pwd TEXT NOT NULL);''')
-    elif role == 1:
-        conn.execute('''CREATE TABLE IF NOT EXISTS accounts
-                    (id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    fname TEXT NOT NULL,
-                    lname TEXT NOT NULL,
-                    cname TEXT NOT NULL UNIQUE,
-                    uname TEXT NOT NULL UNIQUE,
-                    pwd TEXT NOT NULL);''')
-    elif role == 2:
-        conn.execute('''CREATE TABLE IF NOT EXISTS vendor
-                    (id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    fname TEXT NOT NULL,
-                    lname TEXT NOT NULL,
-                    cname TEXT NOT NULL UNIQUE,
-                    vname TEXT NOT NULL UNIQUE,
-                    vmail TEXT NOT NULL UNIQUE,
-                    pwd TEXT NOT NULL);''')
-    cur.execute(f"SELECT * FROM users WHERE uname= ?", (uname,))
+
+    cur.execute(f"SELECT * FROM authorization WHERE user_email= ?", (uname,))
     row = cur.fetchone()
     
     cur.close()
     conn.close()
     
-    if row:
-        if row[-1] == pwd:
-            print("Login Successful", file=sys.stderr)
-            return render_template("dashboard.html")
-        else:
-            print("Login Unsuccessful", file=sys.stderr)
-            return render_template("login.html")
-            
-    else:
+    #username does not exist in the table
+    if not row:
         print(f"No row found with uname = {uname}")
         print("Login Unsuccessful", file=sys.stderr)
         return render_template("login.html")
+        
+    #The username has been found in the table, password does not match  
+    if row[-1] != pwd:
+        print("Login Unsuccessful", file=sys.stderr)
+        return render_template("login.html")
+    
+    #Authentication successful
+    print("Login Successful", file=sys.stderr)
+    
+    conn = sqlite3.connect('users.db')
+    cur = conn.cursor()
+
+    cur.execute(f"select * from authorization a JOIN users on a.auth_id = users.user_id where user_email= ?", (uname,))
+    row = cur.fetchone()
+    print(row, file=sys.stderr)
+    
+    cur.execute(f'''select company_name from (select * from authorization a JOIN users on a.auth_id = users.user_id)
+                    NATURAL JOIN company  where user_email= ?''', (uname,))
+    cname = cur.fetchone()
+    session["cname"] = cname[0]
+    global company_name
+    company_name = cname[0]
+    
+    cur.execute(f'''select company_id from (select * from authorization a JOIN users on a.auth_id = users.user_id)
+                    NATURAL JOIN company  where user_email= ?''', (uname,))
+    cid = cur.fetchone()
+    session["company_id"] = cid[0]
+    global company_id
+    company_id = cid[0]
+    
+    
+    
+    cur.close()
+    conn.close()
+    
+    if row[-1] == 1:
+        print("\nADMIN LOGIN ENTERED\n")
+        return redirect(url_for("dashboard_admin"))
+    elif row[-1] == 2:
+        print("\nApprover LOGIN ENTERED\n")
+        return render_template("dashboard.html")
+    elif row[-1] == 3:
+        print("\nAccounts LOGIN ENTERED\n")
+        return render_template("dashboard.html")
+    else:
+        print("\nVendor LOGIN ENTERED\n")
+        return render_template("dashboard.html")
+    
+
+#DASHBOARD NESTED PAGES --------------------------------
+@app.route('/dashboard_admin')
+def dashboard_admin():
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    
+    # Add column names to the cursor
+    cursor.execute("PRAGMA table_info(users)")
+    column_names = [col[1] for col in cursor.fetchall()]
+
+    # Read the entire table
+    cursor.execute("SELECT * FROM users where company_id=?", (company_id,))
+    data = cursor.fetchall()
+
+    
+
+    conn.close()
+    return render_template('dashboard_admin.html', column_names= column_names, data = data)
+
+@app.route('/adminAddUser')
+def create_user():    
+    return render_template("adminAddUser.html")
+
+
+@app.route('/adminModifyUser',  methods=['POST'])
+def storeAdminAccountData():
+    
+    print("COMPANY NAME IS: ", session.get("cname"), "\n", file = sys.stderr)
+    fname = request.form.get("fname")
+    lname = request.form.get("lname")
+    cname = request.form.get("cname")
+    email = request.form.get("email")
+    contact = request.form.get("contact")
+    pwd = request.form.get("pwd1")
+    role = request.form.get("options")
+    
+    print("ROLE: ", role, sys.stderr)
+    
+    conn = getdbConnection()
+    cur = conn.cursor()
+
+    cur.execute("INSERT INTO authorization(user_email, pwd) VALUES (?, ?)", (email, pwd))
+    conn.commit()
+    auth_id = cur.lastrowid
+    print("auth_id: ", auth_id)
+    # cur.execute("select * from company where company_name = ?;", (session["cname"], ))
+    # company_id = cur.lastrowid
+    print("company_id: ", company_id, sys.stderr)
+
+    cur.execute('''INSERT INTO users(user_id, first_name, last_name, contact, company_id, user_role)
+                    VALUES (?, ?, ?, ?, ?, ?);''', (auth_id, fname, lname, contact, company_id, d[role]))
+    
+    conn.commit()
+    conn.close()
+    
+    return render_template("adminModifyUser.html")
+
+#DASHBOARD NESTED PAGES ENDS--------------------------------
+
+
+@app.route('/dashboard_approver')
+def dashboard_approver():
+    print("UNAME FOUND: ", uname, "\n")
+    column_names = ["ab", "c", "d"]
+    return render_template(url_for('dashboard_admin.html', column_names= column_names))
+
 
 
 @app.route('/dashboard', methods=['POST', 'GET'])
